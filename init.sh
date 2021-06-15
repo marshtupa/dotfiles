9a1c5bf16442cb5c8dc618e28b0bde8b84b363c7
Marshtupa Bogdan <45558274+marshtupa@users.noreply.github.com>
2021-06-15 16:40:46 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -224,6 +224,9 @@ defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -bool true
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Remainders.app"
+dockutil --no-restart --add "/System/Applications/Notes.app"
+dockutil --no-restart --add "/System/Applications/FaceTime.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Safari.app"
